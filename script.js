4f45cb0308aa9d58cd287e24ac2b0569cc335b16
Akshaya Thoutam <akshayathoutam@gmail.com>
2024-08-05 20:12:23 -0400

    fix side bar

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const toggle = document.getElementById('mode-toggle');
+    const toggleIcon = document.getElementById('mode-toggle');
     const body = document.body;
 
     // Check for saved theme preference
     if (localStorage.getItem('theme') === 'light') {
-        toggle.checked = true;
         body.classList.add('light-mode');
         body.classList.remove('dark-mode');
     } else {
-        toggle.checked = false;
         body.classList.add('dark-mode');
         body.classList.remove('light-mode');
     }
 
-    toggle.addEventListener('change', () => {
-        if (toggle.checked) {
+    toggleIcon.addEventListener('click', () => {
+        if (body.classList.contains('dark-mode')) {
             body.classList.add('light-mode');
             body.classList.remove('dark-mode');
             localStorage.setItem('theme', 'light');
